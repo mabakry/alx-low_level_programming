0626765363694e49c2eea995515172cac9c7c2ad
mabakry <mabakry@gmail.com>
2023-08-15 14:38:35 -0700

    555550

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,10 +3,10 @@
 
 	/**
 	 * int print_sign - prints the sign of a number
-	 * Description:  @n: the int to check
-	 * Return: 1 and prints + if n is greater than zero
-	 * 0 and prints 0 if n is zero
-	 * -1 and prints - if n is less than zero
+	 *
+	 * Return: 1 if n is greater than zero
+	 * Return: 0 if n is zero
+	 * Return: '-'1 if n is less than zero
 	 */
 	int print_sign(int n)
 	{
