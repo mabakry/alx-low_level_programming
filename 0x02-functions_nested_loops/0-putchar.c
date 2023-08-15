e799eec24cbbcfcfc7318daadcf803354fae4a5a
mabakry <mabakry@gmail.com>
2023-08-15 06:29:39 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return Always 0 (Success)
+ */
+#define _putchar(c) Putc(c, stdout)
+int main(void)
+{
+	_putchar('_putchar\n');
+	return (0);
+}
