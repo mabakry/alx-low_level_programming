9158d8e55798282ebcffba7762bd64692a6e9821
mabakry <mabakry@gmail.com>
2023-08-15 17:39:55 -0700

    2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
-
-	/**
-	 * print_sign - Entry point 
-	 * Descreption: Determines if the input number.
-	 * @n: the input number as an intger.
-	 * Return: 1 is greater than zero.
-	 * 0 is zero.
-	 *  -1 is less than zero.
+/**
+	 * print_sign - prints the sign of a number
+	 * @n: the int to check
+	 * Return: 1 and prints + if n is greater than zero
+	 * 0 and prints 0 if n is zero
+	 * -1 and prints - if n is less than zero
 	 */
 int print_sign(int n)
 	{
