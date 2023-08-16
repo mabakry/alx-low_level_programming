c63fca6dbecdf8b341a205ff3a16f519946ef391
mabakry <mabakry@gmail.com>
2023-08-15 17:48:12 -0700

    fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-
-	/**
+/**
 	 * main - finds and prints the sum of the even-valued terms
 	 * followed by a new line
 	 * Return: Always 0 (Success)
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-
-	/**
+/**
 	 * main - fibonacci <3
 	 *
 	 * Purpose - no hardcode
