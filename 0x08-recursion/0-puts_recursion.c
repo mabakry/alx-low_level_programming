fec328a8020f6350991f22381d2279c8545f4737
mabakry <mabakry@gmail.com>
2023-08-30 06:53:24 +0000

    00

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- *  _puts_recursion - prints a string
- *  @s: string
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
-		 _putchar(*s);
-		  _puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
