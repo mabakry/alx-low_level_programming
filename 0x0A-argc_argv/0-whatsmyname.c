05c57bbdf1e33ae2f4bb117446302045bef4690a
mabakry <mabakry@gmail.com>
2023-09-01 22:11:17 +0000

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  prints its name
+ *@argc: counter
+ *@argv: string
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+return (0);
+}
