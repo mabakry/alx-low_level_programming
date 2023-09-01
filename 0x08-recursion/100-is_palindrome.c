1e7d9f5a372c39f019cc5a6eea552d86174e5ff4
mabakry <mabakry@gmail.com>
2023-09-01 00:21:04 +0000

    10000

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -26,9 +26,13 @@ return (l);
 int chk(char *s, int i, int l)
 {
 	if (*(s + i) != *(s + l - 1))
-			return (0);
+	{
+		return (0);
+	}
 	if (i >= l)
-			return (1);
+	{
+		return (1);
+	}
 	return (chk(s, i + 1, l - 1));
 }
 #include "main.h"
