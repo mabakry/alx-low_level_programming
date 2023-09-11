5fac0253bc159d312148e6970fbaa3ec184058ab
mabakry <mabakry@gmail.com>
2023-09-11 11:27:31 +0000

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: first member
+ * @age: secomd member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
