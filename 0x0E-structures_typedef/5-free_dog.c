157de2d22acfa015ca047767655b29f3d7f3dda5
mabakry <mabakry@gmail.com>
2023-09-11 14:18:30 +0000

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog -  frees dogs
+ * @d: struct to free
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,5 +21,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *a, char *b);
 int _strlen(char *s);
+void free_dog(dog_t *d);
 
 #endif
