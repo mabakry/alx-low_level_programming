b0a7cf8d071096e8735a67be039078ca836c0327
mabakry <mabakry@gmail.com>
2023-09-11 11:42:11 +0000

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - nitialize a variable of type struct dog
+ * @d: variable
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
