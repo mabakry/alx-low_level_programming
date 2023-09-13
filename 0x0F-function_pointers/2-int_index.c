77874526cad9fc3cb4cb6066d5eb8d82dd7c77b5
mabakry <mabakry@gmail.com>
2023-09-13 19:24:08 +0000

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: arraay
+ * @size: number of elements in the array
+ * @cmp: pointer to function
+ * Return: indexof the first element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || !size || cmp == NULL)
+		return (0);
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -8,5 +8,6 @@ int _putchar(char c);
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
