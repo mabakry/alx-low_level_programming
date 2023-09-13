702dea06110dc9c3b6ef99145e03531de4cd019c
mabakry <mabakry@gmail.com>
2023-09-13 19:29:25 +0000

    task2 fix1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,9 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array == NULL || !size || cmp == NULL)
-		return (0);
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 	while (i < size)
 	{
