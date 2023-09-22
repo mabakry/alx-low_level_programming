0b0d0f018ddda369a7a650dcacd61c3b1cc01841
mabakry <mabakry@gmail.com>
2023-09-22 00:15:46 +0000

    task 5 fix 2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,7 +7,7 @@
  */
 
 __attribute__((constructor))
-	void print_before(void)
+void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
