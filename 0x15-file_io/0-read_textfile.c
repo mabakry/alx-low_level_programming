cab4a13c77cc0906927807718a88374ac757874c
mabakry <mabakry@gmail.com>
2023-10-02 19:21:37 +0000

    task 0 fix 4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,9 +22,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
-
 	x = read(f, buffer, letters);
 	y = write(1, buffer, x);
 
