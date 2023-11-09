749efe72f9b401c69febc00f91dc4358399a19c6
mabakry <mabakry@gmail.com>
2023-11-09 18:56:25 +0000

    task 7 fix 11

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	new->prev = temp;
 	new->next = temp->next;
-	/*temp->next->prev = new;*/
+	temp->next->prev = new;
 	temp->next = new;
 
 	return (new);
