9d5f8ae75c7161d2b0a796ada94984fc8313d629
mabakry <mabakry@gmail.com>
2023-11-13 22:05:01 +0000

    task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+#include "list.h"
+/**
+ * add - add int
+ * @a: input
+ * @b: input
+ * Return : sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub int
+ * @a: input
+ * @b: input
+ * Return : diff
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul int
+ * @a: input
+ * @b: input
+ * Return : mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div int
+ * @a: input
+ * @b: input
+ * Return : div
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod int
+ * @a: input
+ * @b: input
+ * Return : mod
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
