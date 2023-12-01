44cf7de1246511ca3e8aa57588b657664b25ccc4
mabakry <mabakry@gmail.com>
2023-12-01 16:22:56 +0000

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,21 +7,21 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
-        hash_node_t *current, *temp;
-        unsigned long int i;
+	hash_node_t *current, *temp;
+	unsigned long int i;
 
-        for (i = 0; i < ht->size; i++)
-        {
-                current = ht->array[i];
-                while (current)
-                {
-                        temp = current;
-			current = current->next;
-                        free(temp->key);
-                        free(temp->value);
-                        free(temp);
-                }
-        }
-        free(ht->array);
-        free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
